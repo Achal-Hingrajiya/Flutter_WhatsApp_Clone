c767d3f4b8363210a1ebc7a80bca4c8b2c9629f8
Achal Hingrajiya <achalhingrajiya192@gmail.com>
2021-09-14 04:31:23 +0530

    Created Appbar and Tabs

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,31 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  static const MaterialColor greenWA = MaterialColor(
+    // 0xffe55f48, // 0% comes in here, this will be color picked if no shade is selected when defining a Color property which doesn’t require a swatch.
+    0xff075E54,
+    <int, Color>{
+      50: Color(0xFFE1ECEA),
+      100: Color(0xFFB5CFCC),
+      200: Color(0xFF83AFAA),
+      300: Color(0xFF518E87),
+      400: Color(0xFF2C766E),
+      500: Color(0xFF075E54),
+      600: Color(0xFF06564D),
+      700: Color(0xFF054C43),
+      800: Color(0xFF04423A),
+      900: Color(0xFF023129),
+    },
+  );
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: greenWA,
       ),
       home: const MyHomePage(),
     );
@@ -34,6 +43,50 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('WhatsApp'),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: Icon(Icons.search_rounded),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: Icon(Icons.more_vert_rounded),
+            ),
+          ],
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: "CHATS",
+              ),
+              Tab(
+                text: "STATUS",
+              ),
+              Tab(
+                text: "CALLS",
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text("Chat Section"),
+            ),
+            Center(
+              child: Text("Status Section"),
+            ),
+            Center(
+              child: Text("Calls Section"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
